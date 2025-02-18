d9a513593360c33a933fdd971c4c7b0cee316bbc
Leon Holub <leonhol@seznam.cz>
2025-02-18 13:38:19 +0100

    Separated Pixel to own file and enhanced code to use Pixel class

---
+++ Pixel.cs
@@ -0,0 +1,19 @@
+﻿namespace Snake;
+
+public class Pixel
+{
+    public Pixel()
+    {
+    }
+
+    public Pixel(int posX, int posY, ConsoleColor color = ConsoleColor.White)
+    {
+        PosX = posX;
+        PosY = posY;
+        Color = color;
+    }
+
+    public int PosX { get; set; }
+    public int PosY { get; set; }
+    public ConsoleColor Color { get; set; }
+}
